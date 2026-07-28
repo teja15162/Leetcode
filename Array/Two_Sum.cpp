c6e3d82a627406bf2a4a3270fe177edae09f1367
teja15162 <chintala.teja15162@gmail.com>
2026-07-28 20:09:02 +0530

    Q1 (improved) | time :  O(n) | space : O(n)

--- Array/Two_Sum.cpp
+++ Array/Two_Sum.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int, int>> temp;
+        unordered_map<int, int> mp;
 
         for(int i=0;i<nums.size();i++) {
-            temp.push_back({nums[i],i});
+            mp[nums[i]] = i;
         }
 
-        sort(temp.begin(),temp.end());
+        for(int i=0;i<nums.size();i++) {
+            int want = target - nums[i];
 
-        int st = 0;
-        int end = temp.size()-1;
-
-        while(st < end) {
-            int sum = temp[st].first + temp[end].first;
-
-            if(sum == target) {
-                return {temp[st].second, temp[end].second};
-            }
-
-            if(sum < target) {
-                st++;
-            } else {
-                end--;
+            if(mp.find(want) != mp.end() && i != mp[want]) {
+                return {i, mp[want]};
             }
         }
 
