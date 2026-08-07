148e92cad269f3452c82595af011c8e44be45a1c
teja15162 <chintala.teja15162@gmail.com>
2026-08-07 13:00:03 +0530

    Add Container With Most Water solution

---
+++ arrays/0011_container_with_most_water/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int st = 0;
+        int end = height.size()-1;
+        int ar = 0;
+        while(st<=end) {
+            int ht = min(height[st],height[end]);
+            int wd = end - st;
+            ar = max(ar,ht*wd);
+            if(height[st] < height[end]) {
+                st++;
+            } else {
+                end--;
+            }
+        }
+        return ar;
+    }
+};
